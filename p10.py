ef17b67896bb499524eedb7bfd0b5063528edcf0
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-29 10:23:20 +0530

    Create p10.py

---
+++ p10.py
@@ -0,0 +1,21 @@
+'''
+data.info()
+'''
+
+
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 1538 entries, 0 to 1537
+Data columns (total 9 columns):
+ #   Column           Non-Null Count  Dtype  
+---  ------           --------------  -----  
+ 0   ID               1538 non-null   int64  
+ 1   model            1538 non-null   object 
+ 2   engine_power     1538 non-null   int64  
+ 3   age_in_days      1538 non-null   int64  
+ 4   km               1538 non-null   int64  
+ 5   previous_owners  1538 non-null   int64  
+ 6   lat              1538 non-null   float64
+ 7   lon              1538 non-null   float64
+ 8   price            1538 non-null   int64  
+dtypes: float64(2), int64(6), object(1)
+memory usage: 108.3+ KB

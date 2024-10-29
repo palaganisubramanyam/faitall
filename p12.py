1d2f7299b580125a47780ba766051ace2f905a94
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-29 10:24:55 +0530

    Create p12.py

---
+++ p12.py
@@ -0,0 +1,11 @@
+'''
+data.groupby(['previous_owners']).count()
+'''
+
+
+	ID	model	engine_power	age_in_days	km	lat	lon	price
+previous_owners								
+1	1389	1389	1389	1389	1389	1389	1389	1389
+2	117	117	117	117	117	117	117	117
+3	23	23	23	23	23	23	23	23
+4	9	9	9	9	9	9	9	9

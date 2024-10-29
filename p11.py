622e9f95f2bd06709353c07425db8f761f6c4a2a
palaganisubramanyam <subbusubramanyam99553@gmail.com>
2024-10-29 10:24:17 +0530

    Create p11.py

---
+++ p11.py
@@ -0,0 +1,9 @@
+'''
+data.groupby(['model']).count()
+'''
+
+	ID	engine_power	age_in_days	km	previous_owners	lat	lon	price
+model								
+lounge	1094	1094	1094	1094	1094	1094	1094	1094
+pop	358	358	358	358	358	358	358	358
+sport	86	86	86	86	86	86	86	86
